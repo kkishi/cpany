299cf9be3906880d9f6c9487d6706f0d8fee1cb5
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-11-20 22:22:08 +0100

    add solutions

--- pgbattle/2020/3_4/main.cc
+++ pgbattle/2020/3_4/main.cc
@@ -39,15 +39,38 @@ void Main() {
   dfs(db, b, -1, 0);
   int ans = 0;
   rep(2) {
-    rep(p, n) {
-      int pa = da[p];
-      int pb = db[p];
-      // NOTE: The loop below needs to be optimized.
+    const bool naive = false;
+    if (!naive) {
+      // Fast implementation.
+      V<int> v;
       rep(q, n) {
         int qa = da[q];
         int qb = db[q];
-        if (pa < qa && pb >= qb) {
-          ++ans;
+        if (qb < qa) {
+          v.eb(qb);
+        }
+      }
+      sort(v);
+      rep(p, n) {
+        int pa = da[p];
+        int pb = db[p];
+        if (pa < pb) {
+          auto it = lower_bound(all(v), pa - ab + 1);
+          auto jt = lower_bound(all(v), pb + 1);
+          ans += jt - it;
+        }
+      }
+    } else {
+      // Naive implementation.
+      rep(p, n) {
+        int pa = da[p];
+        int pb = db[p];
+        rep(q, n) {
+          int qa = da[q];
+          int qb = db[q];
+          if (pa < qa && pb >= qb) {
+            ++ans;
+          }
         }
       }
     }
